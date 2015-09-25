55b33247a2d8059b2ef5125465ce19789e4ea4b4
Crehop <crehop2@gmail.com>
2015-09-24 20:55:32 -0700

    Updated to basic server

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,15 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 
 public class Server {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		ServerSocket serverSocket = new ServerSocket(12253);
+		Socket socket = serverSocket.accept();
+		Scanner sc = new Scanner(socket.getInputStream());
 	}
 
 }
